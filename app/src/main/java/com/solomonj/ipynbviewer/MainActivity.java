1f95f44e6f5432afa203d389b1f96deb134c0aca
Solo <solomon258@gmail.com>
2023-01-08 17:44:07 +0530

    Final Version 1 without ads

--- app/src/main/java/com/solomonj/ipynbviewer/MainActivity.java
+++ app/src/main/java/com/solomonj/ipynbviewer/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -28,6 +29,17 @@ import com.android.billingclient.api.PurchasesResponseListener;
 import com.android.billingclient.api.PurchasesUpdatedListener;
 import com.android.billingclient.api.QueryProductDetailsParams;
 import com.android.billingclient.api.QueryPurchasesParams;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 
@@ -46,6 +58,10 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
     public static final String MyPRE = "adFlagger" ;
     SharedPreferences sharedPref;
     boolean adFlag;
+    private InterstitialAd mInterstitialAd;
+    private AdView mAdView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +71,21 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         sharedPref = getSharedPreferences(MyPRE, Context.MODE_PRIVATE);
         adFlag = sharedPref.getBoolean("adFlag",false);
         Log.i(TAG, "Flag 1: "+adFlag);
+
+
+        //Adview with flag condition
+        if(adFlag == false){
+            MobileAds.initialize(this, new OnInitializationCompleteListener() {
+                @Override
+                public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+                }
+            });
+            requestNewBanner();
+            requestNewInterstitial();
+        }else{
+            Log.i(TAG,"AdFlag is "+adFlag+" so the ads are not loaded");
+        }
+
 
 
         //initialise button and spinner
@@ -67,7 +98,12 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         removeAds.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                makePurchase();
+                try {
+                    makePurchase();
+                }catch (NullPointerException e){
+                    Log.e(TAG,"Billing error "+e.toString());
+                }
+
             }
         });
 
@@ -95,6 +131,9 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         infologo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(MainActivity.this);
+                }
                 Intent intent = new Intent(getApplicationContext(),information.class);
                 startActivity(intent);
             }
@@ -230,7 +269,7 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
                             }
                         }else{
                             Log.i(TAG,"No Purchases available");
-                            editSharedPref(false);
+                            //editSharedPref(false);
                             adFlag = sharedPref.getBoolean("adFlag",false);
                             Log.i(TAG,"FLAG 5: "+adFlag);
                         }
@@ -263,6 +302,81 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         editor.putBoolean("adFlag",flagValue);
         editor.apply();
         editor.commit();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+
+    //Rectangle Banner Ad
+    private void requestNewBanner(){
+        //Original id unit - ca-app-pub-1715775523919691/5356032797
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    //Interestial ad
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        //Interestial Ad
+        //Original id unit - ca-app-pub-1715775523919691/3195691742
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+                Log.d(TAG, loadAdError.toString());
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                super.onAdLoaded(interstitialAd);
+                mInterstitialAd = interstitialAd;
+                Log.i(TAG, "onAdLoaded");
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdClicked() {
+                        // Called when a click is recorded for an ad.
+                        Log.d(TAG, "Ad was clicked.");
+                    }
+
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // Called when ad is dismissed.
+                        // Set the ad reference to null so you don't show the ad a second time.
+                        Log.d(TAG, "Ad dismissed fullscreen content.");
+                        mInterstitialAd = null;
+                        requestNewInterstitial();
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // Called when ad fails to show.
+                        Log.e(TAG, "Ad failed to show fullscreen content.");
+                        mInterstitialAd = null;
+                    }
+
+                    @Override
+                    public void onAdImpression() {
+                        // Called when an impression is recorded for an ad.
+                        Log.d(TAG, "Ad recorded an impression.");
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // Called when ad is shown.
+                        Log.d(TAG, "Ad showed fullscreen content.");
+                    }
+                });
+            }
+        });
+
     }
 
 }
